ce07cae42717c571c25fea9a55d058b5c5121445
Raviranjan Mishra <raviranjanmishra767@gmail.com>
2025-08-30 13:55:37 +0530

    Delete simple-home-page directory

--- simple-home-page/script.js
+++
@@ -1,7 +0,0 @@
-let round = document.querySelector(".round");
-let body = document.querySelector("body");
-round.addEventListener("click", function () {
-  console.log("ravi aa gaya");
-  round.classList.toggle("roundmove");
-  body.classList.toggle("color");
-});
